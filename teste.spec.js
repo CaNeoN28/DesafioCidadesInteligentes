4519549e43689e313a20af25f4fc406958861e0a
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2023-12-23 21:00:49 -0300

    Inicializando arquivo de teste

---
+++ teste.spec.js
@@ -0,0 +1,7 @@
+const { describe, expect, test } = require("@jest/globals")
+
+describe ("Criação do usuário", () => {
+    test("deve instanciar um usuário com os atributos corretos", () => {
+        expect(true).toBe(true)
+    })
+})
