ebcc4a8cf43c3b3d205bbe854eea246d674367a1
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2023-12-23 21:48:41 -0300

    Validando usuário

--- desafio.js
+++ desafio.js
@@ -1,20 +1,56 @@
 class Usuario {
-    constructor (nome, email, senha, listaDePermissoes, ativo) {
-        this.nome = nome
-        this.email = email
-        this.senha = senha
-        this.listaPermissoes = listaDePermissoes
-        this.ativo = ativo
+  validarUsuario(dadosUsuario) {
+		const {nome, email, senha, listaPermissoes} = dadosUsuario
 
-        this.dataCriacao = new Date()
-        this.dataLogin = null
+    const erros = [];
+    const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const regexSenha =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/g;
+
+		if (nome == undefined){
+			erros.push("Nome é obrigatório")
+		}
+
+    if (email == undefined) {
+      erros.push("Email é obrigatório");
+    } else if (!regexEmail.test(email)) {
+      erros.push("Email inválido");
     }
+
+    if (senha == undefined) {
+      erros.push("Senha é obrigatória");
+    } else if (!regexSenha.test(senha)) {
+			console.log(senha)
+      erros.push("Senha inválida");
+    }
+
+		if (listaPermissoes && !Array.isArray(listaPermissoes)){
+			erros.push("Lista de permissões é inválida")
+		}
+
+    if (erros.length > 0) {
+      throw erros.join(", ");
+    }
+  }
+
+  constructor(nome, email, senha, listaPermissoes, ativo) {
+    try {
+			this.validarUsuario({nome, email, senha, listaPermissoes})
+
+      this.nome = nome;
+      this.email = email;
+      this.senha = senha;
+      this.listaPermissoes = listaPermissoes || [];
+      this.ativo = new Boolean(ativo) || false;
+
+      this.dataCriacao = new Date();
+      this.dataLogin = null;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
-
-const usuario = new Usuario("Carlos", "carlosfelipe.st28@gmail.com", "12345678", [], true)
-
-console.log(usuario)
 
 module.exports = {
-    Usuario
-}
+  Usuario,
+};
--- teste.spec.js
+++ teste.spec.js
@@ -1,13 +1,28 @@
-const { describe, expect, test } = require("@jest/globals")
-const { Usuario } = require("./desafio.js")
+const { describe, expect, test } = require("@jest/globals");
+const { Usuario } = require("./desafio.js");
 
-describe ("Criação do usuário", () => {
-    test("deve instanciar um usuário com os atributos corretos", () => {
-        const usuario = new Usuario("Carlos Felipe", "carlosfelipe.st28@gmail.com", "1234Asdf.", [], true)
+describe("Criação do usuário", () => {
+  test("deve instanciar um usuário com os atributos corretos", () => {
+    const usuario = new Usuario(
+      "Carlos Felipe",
+      "carlosfelipe.st28@gmail.com",
+      "1234Asdf$",
+      [],
+      true
+    );
 
-        expect(usuario.nome).toBe("Carlos Felipe")
-        expect(usuario.email).toBe("carlosfelipe.st28@gmail.com")
-        expect(usuario.listaPermissoes).toEqual([])
-        expect(usuario.ativo).toBe(true)
-    })
-})
+    expect(usuario.nome).toBe("Carlos Felipe");
+    expect(usuario.email).toBe("carlosfelipe.st28@gmail.com");
+    expect(usuario.listaPermissoes).toEqual([]);
+    expect(usuario.ativo).toEqual(new Boolean(true));
+  });
+
+	test("deve dar erro de atributos inválidos", () => {
+		const criarUsuarioErrado = () => {
+			const usuario = new Usuario()
+		}
+
+		expect(criarUsuarioErrado).toThrowError("Nome é obrigatório, Email é obrigatório, Senha é obrigatória")
+	})
+
+});
